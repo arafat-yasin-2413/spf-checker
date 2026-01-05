1e9eb7b5605989970d954fb9a9b1dbaae5c67969
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-05 14:53:30 +0600

    spf checker handler function is ready

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,50 @@
-
+import { useState } from "react";
 
 function App() {
+	
+    const [domain, setDomain] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+    const [spfRecords, setSpfRecords] = useState([]);
+
+    const handleSubmit = async(e) =>{
+        e.preventDefault();
+        setError("");
+        setSpfRecords([]);
+
+        if(!domain.trim()){
+            setError("Please enter a domain name");
+            return ;
+        }
+
+        try{
+            setLoading(true);
+            const res = await fetch(`http://localhost:5000/spf?domain=${domain}`);
+            const data = await res.json();
+
+            console.log('-------------printing response --------------')
+            console.log(data);
+            console.log('---- response ends here ----');
+            if(!res.ok){
+                throw new Error(data.message);
+            }
+
+            setSpfRecords(data.spfRecords);
+        }
+        catch(err){
+            setError(err.message || "Something went wrong");
+        }
+        finally{
+            setLoading(false);
+        }
+    };
 
 	return (
 		<>
-			<h1 className="text-2xl text-red-500">Hello world</h1>
-            <h4>Hello world</h4>
-		</>
+            <div>
+                SPF checker form
+            </div>
+        </>
 	);
 }
 
